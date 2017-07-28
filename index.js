a27f0263eae663177a5e63468a51ab308b82419a
ramoona <lindarian12@gmail.com>
2017-07-28 17:07:54 +0300

    v1.0.3

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var storageHistory = sessionStorage.getItem('session-history');
+var scrollHistory = [];
+
 var sessionHistory = {
     length: 0,
     routes: []
 };
 
 if (storageHistory) {
-    sessionHistory = JSON.parse(storageHistory);
+    try {
+        sessionHistory = JSON.parse(storageHistory);
+    } catch (e) {
+        console.warn('Session history parse failed:', e);
+    }
 }
 
 module.exports = function (_ref) {
@@ -16,11 +22,28 @@ module.exports = function (_ref) {
                 var route = '' + location.pathname + location.search;
                 var lastRoute = sessionHistory.routes[sessionHistory.length - 1];
 
+                if (history.action === 'PUSH') {
+                    scrollHistory.push(window.scrollY);
+                }
+
                 if (route !== lastRoute) {
                     sessionHistory.length++;
                     sessionHistory.routes.push(route);
                     sessionStorage.setItem('session-history', JSON.stringify(sessionHistory));
                 }
+            }
+        },
+        render: function () {
+            if (history.internalBack) {
+                var position = scrollHistory[scrollHistory.length - ++history.internalBack];
+
+                // temp stuff, waiting for render hooks fix
+                setTimeout(function () {
+                    if (typeof position !== 'undefined') {
+                        window.scrollTo(0, position);
+                    }
+                }, 100);
+                history.internalBack = null;
             }
         }
     };
