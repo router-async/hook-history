759c3143d9f999a3e41518ed059d2afa04da335d
ramoona <lindarian12@gmail.com>
2017-07-28 16:47:04 +0300

    add scroll restoration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 const storageHistory = sessionStorage.getItem('session-history');
+const scrollHistory = [];
+
 let sessionHistory = {
     length: 0,
     routes: []
 };
 
 if(storageHistory) {
-    sessionHistory = JSON.parse(storageHistory)
+    try {
+        sessionHistory = JSON.parse(storageHistory);
+    } catch(e) {
+        console.warn('Session history parse failed:', e);
+    }
 }
 
 module.exports = ({ history }) => ({
@@ -14,11 +20,26 @@ module.exports = ({ history }) => ({
             const route = `${location.pathname}${location.search}`;
             const lastRoute = sessionHistory.routes[sessionHistory.length - 1];
 
+            if(history.action === 'PUSH') {
+                scrollHistory.push(window.scrollY);
+            }
+
             if(route !== lastRoute) {
                 sessionHistory.length++;
                 sessionHistory.routes.push(route);
                 sessionStorage.setItem('session-history', JSON.stringify(sessionHistory));
             }
+        }
+    },
+    render: () => {
+        if(history.internalBack) {
+            const position = scrollHistory[scrollHistory.length - ++history.internalBack];
+
+            // temp stuff, waiting for render hooks fix
+            setTimeout(() => {
+                if(typeof position !== 'undefined') { window.scrollTo(0, position); }
+            }, 100);
+            history.internalBack = null;
         }
     }
 });
