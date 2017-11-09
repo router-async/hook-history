275099000845ae84e4e57f933580d7d5c6109713
lamo2k123 <lamo2k123@gmail.com>
2017-11-09 14:18:42 +0300

    Fixed back history & up version

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 const storageHistory = sessionStorage.getItem('session-history');
-const scrollHistory = [];
 
-let sessionHistory = {
-    length: 0,
-    routes: []
-};
+let session = [];
 
 if(storageHistory) {
     try {
-        sessionHistory = JSON.parse(storageHistory);
+        session = JSON.parse(storageHistory);
     } catch(e) {
         console.warn('Session history parse failed:', e);
     }
@@ -17,29 +13,21 @@ if(storageHistory) {
 module.exports = ({ history }) => ({
     start: () => {
         if(history.action) {
-            const route = `${location.pathname}${location.search}`;
-            const lastRoute = sessionHistory.routes[sessionHistory.length - 1];
+            const next = location.pathname + location.search;
+            const prev = session[session.length - 1];
 
-            if(history.action === 'PUSH') {
-                scrollHistory.push(window.scrollY);
+            switch(history.action) {
+                case 'PUSH':
+                    if (next !== prev) {
+                        session.push(next);
+                        sessionStorage.setItem('session-history', JSON.stringify(session));
+                    }
+                    break;
+                case 'POP':
+                    session.pop();
+                    sessionStorage.setItem('session-history', JSON.stringify(session));
+                    break;
             }
-
-            if(route !== lastRoute) {
-                sessionHistory.length++;
-                sessionHistory.routes.push(route);
-                sessionStorage.setItem('session-history', JSON.stringify(sessionHistory));
-            }
-        }
-    },
-    render: () => {
-        if(history.internalBack) {
-            const position = scrollHistory[scrollHistory.length - ++history.internalBack];
-
-            // temp stuff, waiting for render hooks fix
-            setTimeout(() => {
-                if(typeof position !== 'undefined') { window.scrollTo(0, position); }
-            }, 100);
-            history.internalBack = null;
         }
     }
 });
